64e960ac5b30392d095aa4204a66aabf0e065bac
Antoon Decoussemaeker <dantoon@student.ethz.ch>
2018-06-05 11:48:18 +0200

    added controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include "controller.h"
+#include <tf/transform_datatypes.h>
 
 int main(int argc, char **argv){
 
@@ -7,15 +8,47 @@ int main(int argc, char **argv){
 	ros::NodeHandle nh;
 	controller robot_ctrl(nh);
 	
-	ros::Rate pubRate(1);
+	ros::Rate samplingRate(1/robot_ctrl.Ts);
+	ros::Rate spinRate(60);
 	
-  while(ros::ok){
+	float err, ctrl;
+  geometry_msgs::Pose pathPose;
+
+  while(ros::ok()){
     ros::spinOnce();
-    while(robot_ctrl.cmdOk){
-    	printf("publishing cmds...\n");
-      pubRate.sleep();
-      robot_ctrl.cmdPub();
+    
+    if(robot_ctrl.cmdOk){
+      robot_ctrl.pathStartSet(&pathPose);
+      ros::Time start = ros::Time::now();
+      
+      while(robot_ctrl.cmdOk && ros::ok()){
+        samplingRate.sleep();
+        /*
+        pathPose = robot_ctrl.pathPoseSet(pathPose);
+        err = robot_ctrl.error(pathPose);
+        ctrl = robot_ctrl.pid(err);
+        printf("error: %f  controller output: %f\n", err, ctrl);
+        robot_ctrl.cmdPub(ctrl);
+        */
+        
+        ctrl = 0;
+        pathPose = robot_ctrl.pathPoseSet(pathPose);
+        ros::spinOnce();
+        err = robot_ctrl.error(pathPose);
+        robot_ctrl.pid(&robot_ctrl.ctrlDeviation);
+        robot_ctrl.pid(&robot_ctrl.ctrlSpeed);
+        robot_ctrl.pid(&robot_ctrl.ctrlAngle);
+        //printf("error: %f  controller output: %f\n", err, ctrl);
+        robot_ctrl.cmdPub(ctrl);
+        
+        
+        if(ros::Time::now().toSec() - start.toSec() >= robot_ctrl.pubCounter + 1){ //TODO increase pubCounter when robot is at the end of first command
+          robot_ctrl.pubCounter++;
+          printf("%i\n", robot_ctrl.pubCounter);
+        }
+      }
     }
+    spinRate.sleep();
   }
 }
 
@@ -25,6 +58,7 @@ int main(int argc, char **argv){
 void controller::cmdNumCallback(const std_msgs::Int16& msg){
   cmdNum = msg.data;
   pathCmds = new geometry_msgs::Twist[cmdNum];
+  pathCmdsCpy = new geometry_msgs::Twist[cmdNum];
 }
 
 void controller::cmdCallback(const geometry_msgs::Twist& msg){
@@ -49,31 +83,142 @@ void controller::cancelCallback(const std_msgs::Int16& msg){
 	printf("path cancelled.\n");
 }
 
-//-----Functions------
-//--------------------
-float controller::pidControl(float y, float x){
-	error[errCounter] = y - x;
-	//I = sum(error);
-	//D = (error[errCounter]-error[errCounter-1])/Ts;
-	//P = error[errCounter];
-	//ctrl = Kp*P + Ki*I + Kd*D;
+void controller::odomCallback(const nav_msgs::Odometry& msg){
+  currentOdom = msg;  
 }
 
+void controller::mapMetaCallback(const nav_msgs::MapMetaData& msg){
+  mapMeta = msg;
+}
+//-----Functions------
+//--------------------
+void controller::pathStartSet(geometry_msgs::Pose* startPose){
+  *startPose = currentOdom.pose.pose;
+}
+
+geometry_msgs::Pose controller::pathPoseSet(geometry_msgs::Pose startPose){
+  //TODO: check multiple points and see which is the closest -> use that one to calculate d
+  
+  float w = pathCmds[pubCounter].angular.z;
+  float v = pathCmds[pubCounter].linear.x / mapMeta.resolution;
+  float alpha1 = tf::getYaw(startPose.orientation);
+  
+  float endX = startPose.position.x - v/w * sin(alpha1) + v/w * sin(alpha1 + w*Ts);
+  float endY = startPose.position.y + v/w * cos(alpha1) - v/w * cos(alpha1 + w*Ts);
+  float alpha2 = tf::getYaw(startPose.orientation) + w*Ts;
+  
+  geometry_msgs::Pose endPose;
+  endPose.position.x = endX;
+  endPose.position.y = endY;
+  
+  tf::Quaternion q_end = tf::createQuaternionFromRPY(0,0,alpha2);
+  q_end.normalize();
+	tf::quaternionTFToMsg(q_end, endPose.orientation);
+	
+  return endPose;
+}
+/*
+float controller::error(geometry_msgs::Pose pathPose){
+  ros::spinOnce();
+  float dx = pathPose.position.x - currentOdom.pose.pose.position.x;
+  float dy = pathPose.position.y - currentOdom.pose.pose.position.y;
+  float d = sqrt(dx*dx+dy*dy);
+  float alpha = atan2(dy,dx) - tf::getYaw(currentOdom.pose.pose.orientation);
+  //int sign = (alpha > 0) - (alpha < 0);
+  float error = d * sin(alpha)/10;
+  
+  error = Ts*(tf::getYaw(pathPose.orientation) - tf::getYaw(currentOdom.pose.pose.orientation)); //Only compares angles
+  return error;
+}*/
+
+float controller::error(geometry_msgs::Pose pathPose){ //error for angle, deviation and speed
+  float x, xr, y ,yr, theta, thetar;
+  
+  xr = pathPose.position.x;
+  yr = pathPose.position.y;
+  thetar = tf::getYaw(pathPose.orientation);
+  
+  x = currentOdom.pose.pose.position.x;
+  y = currentOdom.pose.pose.position.y;
+  theta = tf::getYaw(currentOdom.pose.pose.orientation);
+  
+  float e1, e2, e3;
+  
+  e1 = -cos(theta) * (x-xr) - sin(theta) * (y-yr);
+  e2 = sin(theta) * (x-xr) - cos(theta) * (y-yr);
+  e3 = -(theta-thetar);
+  
+  float ur1, ur2, ue1, ue2, k1, k2, k3, xi, g, sign_ur1;
+  
+  xi = 0.6;
+  g = 2;
+  ur1 = pathCmds[pubCounter].linear.x;
+  ur2 = pathCmds[pubCounter].angular.z;
+  
+  k1 = 2 * xi * sqrt(ur2*ur2 + g*ur1*g*ur1);
+  k2 = g * abs(ur1);
+  k3 = k1;
+  
+  sign_ur1 = (ur1 > 0) - (ur1 < 0);
+  ue1 = -k1 * e1;
+  ue2 = -sign_ur1 * k2 * e2 - k3 * e3;
+  
+  float v, w;
+  
+  v = cos(e3) * ur1 - ue1;
+  w = ur2 - ue2;
+  
+  pathCmdsCpy[pubCounter].linear.x = v;
+  pathCmdsCpy[pubCounter].angular.z = w;
+  
+  return 0;
+}
+/*
+float controller::pid(float error){
+	I = I + Ts*error;
+	D = (error-oldError)/Ts;
+	P = error;
+	oldError = error;
+	float ctrl = Kp*P + Ki*I + Kd*D;
+	return ctrl;
+}*/
+
+float controller::pid(controlValue* control){
+  control->P = control->error;
+	control->I = control->I + Ts * control->error;
+	control->D = (control->error - control->oldError) / Ts;
+
+	control->ctrl = control->Kp * control->P + control->Ki * control->I + control->Kd * control->D;
+	return 0.0f;
+}
 
 //-----Publishers-----
 //--------------------
-void controller::cmdPub(){
+/*
+void controller::cmdPub(float ctrl){
+  pathCmds[pubCounter].angular.z += ctrl;
   pubCmdVel.publish(pathCmds[pubCounter]);
-  pubCounter++;
   
   if(pubCounter == cmdNum){
     pubCounter = 0;
     cmdOk = false;
     printf("publishing finished.\n");
   }
+}*/
+
+void controller::cmdPub(float ctrl){
+  pathCmdsCpy[pubCounter].angular.z;
+  pathCmdsCpy[pubCounter].linear.x;
+  pubCmdVel.publish(pathCmds[pubCounter]);
+  //printf("ctrlDev: %f   ctrlSpeed: %f   ctrlAngle: %f\n", ctrlDeviation.ctrl, ctrlSpeed.ctrl, ctrlAngle.ctrl);
+  printf("linear x: %f   angular z: %f\n", pathCmds[pubCounter].linear.x, pathCmds[pubCounter].angular.z);
+    
+  if(pubCounter == cmdNum){
+    pubCounter = 0;
+    cmdOk = false;
+    printf("publishing finished.\n");
+  }
 }
-
-
 
 
 
--- src/controller.h
+++ src/controller.h
@@ -2,38 +2,62 @@
 #include "std_msgs/Int16.h"
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Point.h"
+#include "geometry_msgs/Pose.h"
+#include "nav_msgs/Odometry.h"
+#include "nav_msgs/MapMetaData.h"
 
 class controller{
 
 	public:
 		controller(ros::NodeHandle);
     ~controller(){}
-    
+	  float Ts;
     bool cmdOk;
-    
-    void cmdPub();
+		int pubCounter;
+
+    void pathStartSet(geometry_msgs::Pose*);    
+    geometry_msgs::Pose pathPoseSet(geometry_msgs::Pose);
+		float error(geometry_msgs::Pose);
+		//float pid(float);   
+    void cmdPub(float);
+	  
+	  struct controlValue{
+	    float P, I, D;
+	    float Kp, Ki, Kd;
+	    float oldError;
+	    float error;
+	    float ctrl;
+	    controlValue();
+	   };
+	  
+	  controlValue ctrlDeviation, ctrlSpeed, ctrlAngle;
+	  float pid(controlValue*);
 	
 	private:
-		int errCounter;
-		float error[];
+    float oldError;
 		float Kp, Ki, Kd;
-		float pidControl(float, float);
+		float P, I, D;
 	
 		int cmdCounter;
-		int pubCounter;
 	  int cmdNum;
 		geometry_msgs::Twist* pathCmds;
-		geometry_msgs::Point closestPoint1;
-		geometry_msgs::Point closestPoint2;
+		geometry_msgs::Twist* pathCmdsCpy;
+		
+		nav_msgs::MapMetaData mapMeta;
+		nav_msgs::Odometry currentOdom;
 		
 		ros::Subscriber subCmdNum;
 		ros::Subscriber subCmd;
 		ros::Subscriber subCancel;
+		ros::Subscriber subOdom;
+		ros::Subscriber subMapMeta;
 		ros::Publisher pubCmdVel;
 		
 		void cmdNumCallback(const std_msgs::Int16&);
 		void cmdCallback(const geometry_msgs::Twist&);
 		void cancelCallback(const std_msgs::Int16&);
+		void odomCallback(const nav_msgs::Odometry&);
+		void mapMetaCallback(const nav_msgs::MapMetaData&);
 };
 
 //----Constructors----
@@ -45,14 +69,34 @@ controller::controller(ros::NodeHandle nh){
 	cmdOk = false;
 	cmdNum = 0;
 	
+	Ts = 0.02f;
+	oldError = 0.0f;
+	Kp = 0.5f;
+	Ki = 0.02f;
+	Kd = 0.02f;
+	P = 0.0f;
+	I = 0.0f;
+	D = 0.0f;
+	
   subCmdNum = nh.subscribe("/cmdNum", 10, &controller::cmdNumCallback, this);
   subCmd = nh.subscribe("/cmds", 10, &controller::cmdCallback, this);
   subCancel = nh.subscribe("/cancel_rrt", 10, &controller::cancelCallback, this);
+  subOdom = nh.subscribe("/robot_0/odom", 10, &controller::odomCallback, this);
+  subMapMeta = nh.subscribe("/map_metadata", 10, &controller::mapMetaCallback, this);
 
   pubCmdVel = nh.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",50,true);
 }
 
-
+controller::controlValue::controlValue(){
+  error = 0.0f;
+	oldError = 0.0f;
+	Kp = 0.5f;
+	Ki = 0.02f;
+	Kd = 0.02f;
+	P = 0.0f;
+	I = 0.0f;
+	D = 0.0f;  
+}
 
 
 
